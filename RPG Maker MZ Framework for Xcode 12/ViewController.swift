05554d840b2ab05f022cfe8689185b502c128486
laguna783 <Giancarlos.Alvarado@gmail.com>
2021-02-05 11:36:25 -0800

    Initial Commit

---
+++ RPG Maker MZ Framework for Xcode 12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RPG Maker MZ Framework for Xcode 12
+//
+//  Created by Giancarlos Alvarado on 2/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
