7eebbcc2b239d546a313bd1fe4d1ed04693b7fc4
laguna783 <Giancarlos.Alvarado@gmail.com>
2021-02-07 01:44:02 -0800

    With this framework, it is possible to include features such as multiple RPG Maker games in one app and theoretically add features such as in-app purchasing, subscriptions, etc., before the RPG Maker MZ game actually launches.

---
+++ RPG Maker MZ Framework for Xcode 12/ViewControllerMain.swift
@@ -0,0 +1,18 @@
+//
+//  ViewControllerMain.swift
+//  RPG Maker MZ Framework for Xcode 12
+//
+//  Created by Giancarlos Alvarado on 2/6/21.
+//
+// This file represents the first ViewController that launches with the app. From here you have the option of creating your own custom pre-app menu in Xcode and Swift.
+// With this framework, it is possible to include features such as multiple RPG Maker games in one app and theoretically add features such as in-app purchasing, subscriptions, etc., before the RPG Maker MZ game actually launches.
+
+import UIKit
+import WebKit
+
+class ViewControllerMain: UIViewController, WKNavigationDelegate, WKUIDelegate {
+    
+    
+
+}
+
